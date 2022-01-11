ed23669049912a1d5a982ad4f3d01517c897bc93
pollohultima <matteo.polonio@gmail.com>
2022-01-11 10:20:00 +0100

    made controller

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>My Library</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime optio, magnam culpa suscipit earum maiores blanditiis totam! Reprehenderit eius, harum magnam, dolor reiciendis, at vel corrupti consectetur sunt nostrum consequuntur.</p>
+
+        <section class="books">
+            <h3>Our Books</h3>
+        </section>
+    </div>
+@endsection
